ba6061f06190517e8b93ef8b20a5409cdf7590ed
Robert Tanase <robert@crowdconnected.com>
2020-04-14 17:18:21 +0100

    Initial Commit

---
+++ Gusto Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gusto Demo
+//
+//  Created by TCode on 14/04/2020.
+//  Copyright © 2020 TCode. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Gusto DemoTests/Gusto_DemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  Gusto_DemoTests.swift
+//  Gusto DemoTests
+//
+//  Created by TCode on 14/04/2020.
+//  Copyright © 2020 TCode. All rights reserved.
+//
+
+import XCTest
+@testable import Gusto_Demo
+
+class Gusto_DemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
